7e9c6d592659f05942dfddf61cf750a249f1090a
Vino <vinofernando84@gmail.com>
2026-03-16 22:00:33 +0700

    add linkedin

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,25 +1,31 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 export default function Contact() {
   return (
     <section className="">
       <div className="container">
-        <h1>
-          Contact
-        </h1>
+        <h1>Contact</h1>
         <p>
-          Hello, my name is VinoFernando, start my journey in fullstack web developer.
-          Experienced build a simple authentication for backend using node and express.
-          Here are some projects that I have made, some from bootcamps and personal projects :
+          Hello, my name is VinoFernando, start my journey in fullstack web
+          developer. Experienced build a simple authentication for backend using
+          node and express. Here are some projects that I have made, some from
+          bootcamps and personal projects :
         </p>
 
         <a
           href="https://github.com/Vinofernando?tab=repositories"
-          target='_blank'
-          rel='noopener noreferrer'
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          <FontAwesomeIcon icon={faGithub} size="2x" className='icon'/>
+          <FontAwesomeIcon icon={faGithub} size="2x" className="icon" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/vino-fernando-b1b66a274/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faLinkedin} size="2x" className="icon" />
         </a>
       </div>
     </section>
